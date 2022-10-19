2f2d793337ac5e97b4a60698660092448d68a260
SamuelWhittle <swbnp7@gmail.com>
2022-10-18 21:22:09 -0500

    added calc_corner_coords

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,82 @@
 use wasm_bindgen::prelude::*;
-use js_sys::Array;
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet(message: &str) {
-    alert(message);
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
 pub struct PerlinNoise {
-    grid_dims: Vec<f64>,
-    num_octaves: f64,
-    octave_scale: f64,
-    unit_corners: Vec<u8>
+    grid_dims: Vec<usize>,
+    num_dims: u32,
+    num_octaves: u32,
+    octave_scale: f32,
+    seed: f32,
+    unit_corners: Vec<u32>
 }
 
 #[wasm_bindgen]
 impl PerlinNoise {
-    pub fn define_multi_octave(dims_js_arr: Array, num_octaves: f64, octave_scale: f64) -> PerlinNoise {
-        let grid_dims = PerlinNoise::get_vec_from_js_array(dims_js_arr);
+    pub fn define_multi_octave(grid_dims: Vec<usize>, num_octaves: u32, octave_scale: f32, seed: f32) -> PerlinNoise {
         let unit_corners = PerlinNoise::calc_unit_corners(grid_dims.len());
 
         PerlinNoise {
-            grid_dims,
+            grid_dims: grid_dims.clone(),
+            num_dims: grid_dims.len() as u32,
             num_octaves,
             octave_scale,
+            seed,
             unit_corners,
         }
     }
 
-    pub fn define_single_octave(dims_js_arr: Array) -> PerlinNoise {
-        let grid_dims = PerlinNoise::get_vec_from_js_array(dims_js_arr);
+    pub fn define_single_octave(grid_dims: Vec<usize>, seed: f32) -> PerlinNoise {
         let unit_corners = PerlinNoise::calc_unit_corners(grid_dims.len());
 
         PerlinNoise {
-            grid_dims,
-            num_octaves: 1.0,
+            grid_dims: grid_dims.clone(),
+            num_dims: grid_dims.len() as u32,
+            num_octaves: 1,
             octave_scale: 1.0,
+            seed,
             unit_corners,
         }
     }
 
-    pub fn get_noise_value(&self, coords_js_arr: Array) -> u32 {
-        let mut coords = PerlinNoise::get_vec_from_js_array(coords_js_arr);
+    pub fn get_noise_value(&self, coords: Vec<f32>) -> f32 {
 
-        0
+        0.0
     }
 
-    pub fn calc_unit_corners(num: usize) -> Vec<u8>{
-        let corner_count = 2u8.pow(num as u32);
+    pub fn calc_corner_coords(&self, coords: Vec<f32>) -> Vec<u32> {
+        let corner_count = 2u32.pow(self.num_dims);
+
+        let mut corner_coords: Vec<u32> = Vec::new();
+
+        for corner in 0..corner_count {
+            for dim in 0..self.num_dims {
+                corner_coords.push(coords[dim as usize].floor() as u32 + self.unit_corners[(corner * 3 + dim) as usize]);
+            }
+        }
+
+        corner_coords
+    }
+
+    pub fn calc_unit_corners(num_dims: usize) -> Vec<u32>{
+        let corner_count = 2u32.pow(num_dims as u32);
 
         let mut unit_corners = Vec::new();
 
         for corner in 0..corner_count {
-            for bit in (0..num).rev() {
+            for bit in (0..num_dims).rev() {
                 unit_corners.push(corner >> bit & 1);
             }
         }
@@ -65,42 +84,33 @@ impl PerlinNoise {
         unit_corners
     }
 
-    pub fn get_vec_from_js_array(arr: Array) -> Vec<f64> {
-        let mut vec: Vec<f64> = Vec::new();
-
-        for i in 0..arr.length() {
-            match arr.get(i).as_f64() {
-                None => {},
-                Some(coord) => {
-                    vec.push(coord);
-                }
-            }
-        }
-
-        vec
-    }
-
-    pub fn get_unit_corners(&self) -> Vec<u8> {
-        self.unit_corners.clone()
-    }
-
-    pub fn get_grid_dims(&self) -> Vec<f64> {
+    // BASIC GETS
+    pub fn get_grid_dims(&self) -> Vec<usize> {
         self.grid_dims.clone()
     }
 
-    pub fn get_num_octaves(&self) -> f64 {
+    pub fn get_num_octaves(&self) -> u32 {
         self.num_octaves
     }
 
-    pub fn get_octave_scale(&self) -> f64 {
+    pub fn get_octave_scale(&self) -> f32 {
         self.octave_scale
     }
 
-    pub fn set_num_octaves(&mut self, num_octaves: f64) {
+    pub fn get_unit_corners(&self) -> Vec<u32> {
+        self.unit_corners.clone()
+    }
+
+    pub fn get_num_dims(&self) -> u32 {
+        self.num_dims
+    }
+
+    // BASIC SETS
+    pub fn set_num_octaves(&mut self, num_octaves: u32) {
         self.num_octaves = num_octaves
     }
 
-    pub fn set_octave_scale(&mut self, octave_scale: f64) {
+    pub fn set_octave_scale(&mut self, octave_scale: f32) {
         self.octave_scale = octave_scale
     }
 }
